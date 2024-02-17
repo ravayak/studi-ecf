ac53baac0a785727b2d2077027015ba0b7dcdaab
ravayak <antonny.kihm@protonmail.com>
2024-02-17 18:33:40 +0100

    updated Apache Spark main && 'ecf final.doc'

--- Apache Spark/main.tf
+++ Apache Spark/main.tf
@@ -17,7 +17,6 @@ module "security" {
 module "emr" {
   source                    = "./modules/emr"
   name                      = "${var.name}"
-  release_label             = "${var.release_label}"
   applications              = "${var.applications}"
   subnet_id                 = "${var.subnet_id}"
   key_name                  = "${var.key_name}"
@@ -26,9 +25,4 @@ module "emr" {
   core_instance_type        = "${var.core_instance_type}"
   core_instance_count       = "${var.core_instance_count}"
   core_ebs_size             = "${var.core_ebs_size}"
-  emr_master_security_group = "${module.security.emr_master_security_group}"
-  emr_slave_security_group  = "${module.security.emr_slave_security_group}"
-  emr_ec2_instance_profile  = "${module.iam.emr_ec2_instance_profile}"
-  emr_service_role          = "${module.iam.emr_service_role}"
-  emr_autoscaling_role      = "${module.iam.emr_autoscaling_role}"
 }
