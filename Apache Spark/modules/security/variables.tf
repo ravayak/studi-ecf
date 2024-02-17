af5c4f3465e87c8c079ba8bb81abd51cf9ed0a32
ravayak <antonny.kihm@protonmail.com>
2024-02-17 18:41:28 +0100

    updated Apache Spark variables

--- Apache Spark/modules/security/variables.tf
+++ Apache Spark/modules/security/variables.tf
@@ -1,3 +1,3 @@
-variable "name" {}
-variable "vpc_id" {}
-variable "ingress_cidr_blocks" {}
+variable "name" {default = "spark-s3-studi-security-ecf"}
+variable "vpc_id" {default="vpc-19bb9770"}
+variable "ingress_cidr_blocks" {default="0.0.0.0/0"}
